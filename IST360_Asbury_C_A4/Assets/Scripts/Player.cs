3f00da3673ae8e73ce6a14c4fe79031c715bc15e
Cameron Asbury <asbury56@marshall.edu>
2016-12-15 18:07:24 -0500

    A4 Almost done

---
+++ IST360_Asbury_C_A4/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	//Consitant Updates instead of updates per frame
+	void FixedUpdate () {
+        movement();
+        //Debug.Log("Working maybe?");
+    }
+
+    /// <summary>
+    /// Controls Movement
+    /// Normalizes Vectors so Diagonal movement stays the same
+    /// </summary>
+    void movement()
+    {
+        Vector3 translation = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+            translation += new Vector3(0, .1f, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            translation += new Vector3(0, -.1f, 0);
+
+        if (Input.GetKey(KeyCode.A))
+            translation += new Vector3(-.1f, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+            translation += new Vector3(.1f, 0, 0);
+
+        //Normalize "normalizes" the vectors all to the same value
+        //This enables my player to travel diagonally at the correct speed
+        translation.Normalize();
+        translation *= speed;
+        transform.position += translation;
+
+    }
+}
---
+++ IST360_Asbury_C_A4/Assets/Scripts/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject moneySpawner;
+    public float spawnTime;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ObjectSpawner());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator ObjectSpawner()
+    {
+        while (true)
+        {
+
+            GameObject go = new GameObject();
+            go = (GameObject)Instantiate(moneySpawner, transform.position, transform.rotation);
+           go.transform.position += new Vector3(Random.Range(-8.5f, 8.5f), 0);
+           yield return new WaitForSeconds(spawnTime);
+        }
+    }
+}
